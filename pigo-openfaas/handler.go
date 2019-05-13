e0ab26f1e3f24816d52415d95a7ff3d77b371a80
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-13 15:37:18 -0300

    Remove lazy comments

--- pigo-openfaas/handler.go
+++ pigo-openfaas/handler.go
@@ -58,9 +58,9 @@ type DetectionResult struct {
 
 // Handle a serverless request
 func Handle(req http.Request) ([]byte, error) {
-	//before := time.Now()
-	output := OldHandle()
-	//output := time.Since(before).Nanoseconds()
+	before := time.Now()
+	OldHandle()
+	output := time.Since(before).Nanoseconds()
 	return []byte(fmt.Sprintf("%v", output)), nil
 }
 
